257a164c1f3b1068a31c85bf41cbce28124112d0
vijay sani <vijay.sani112@gmail.com>
2015-07-20 01:21:10 +0530

    Adding encapsulation for puzzlemanager

--- 15-puzzle.js
+++ 15-puzzle.js
@@ -1,3 +1,5 @@
+(function(){
+
 var puzzleManger;
 puzzleManger = {
   sequenceArray: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
@@ -140,4 +142,8 @@ puzzleManger = {
 
   }
 }
+
 puzzleManger.init();
+	
+	
+})();
